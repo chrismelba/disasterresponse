b4e9c6e026e349acb8573948995bc960a4f3b696
Chris <chrismelba@gmail.com>
2019-04-08 11:12:51 +0000

    created etl_pipeline.py

---
+++ app/etl_pipeline.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Program to train a Neural Net on your data with various options',
+)
+parser.add_argument('-messages', action='store', type=str, help = 'The messages file to be import', default = 'messages.csv')
+parser.add_argument('-categories', action = 'store', type = str, help = 'The category file to be import', default = 'categories.csv')
+
+args = parser.parse_args()
+
+messages = pd.read_csv(vars(args)['messages'])
+categories = pd.read_csv(vars(args)['categories'])
+df = messages.merge(categories, on='id')
+categories = df.categories.str.split(';', expand = True)
+row = categories.iloc[0, :]
+category_colnames = list(map((lambda x: x[:-2]), row))
+categories.columns = category_colnames
+
+for column in categories:
+    # set each value to be the last character of the string
+    categories[column] = list(map(lambda x: x[-1:], categories[column]))
+    
+    # convert column from string to numeric
+    categories[column] = list(map(int, categories[column]))
+categories.head()
+
+df.drop('categories', axis = 1, inplace = True)
+df = pd.concat([df, categories], axis = 1)
+df.drop_duplicates(inplace = True)
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///disaster.db')
+df.to_sql('messages', engine, index=False)
